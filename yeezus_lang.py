c37b8322c5f2a14ba65cdef0ca29bf80173b056e
Akil Harris <akil.harris@gmail.com>
2014-10-29 20:00:41 -0400

    initial lang exercises

---
+++ yeezus_lang.py
@@ -0,0 +1,9 @@
+from __future__ import division
+import nltk
+import re
+import pprint
+
+with open('/Users/akilharris/projects/yeezylyrics/lyricsKanye-west-never-let-me-down-lyrics.txt') as f:
+    rawfile = f.read()
+
+print(rawfile)
