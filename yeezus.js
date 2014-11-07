d094f259f06d4acde0d18585e8d4324fea4df577
Akil Harris <akil.harris@gmail.com>
2014-11-06 21:47:17 -0500

    server path fix

--- yeezus.js
+++ yeezus.js
@@ -39,11 +39,11 @@ if (env_mode === 'dev') {
 }
 
 if (env_mode === 'production') {
-  app.use(express.static(__dirname + '/dist'));
+  app.use(express.static('../dist'));
   //app.use(express.static(__dirname + '/.tmp'));
   app.use(app.router);
   app.get('/', function (request, response) {
-    response.sendfile('/dist/index.html');
+    response.sendfile('../dist/index.html');
   });
   app.post('/yeezus', function (request, response) {
     handlers.handlePost(request, response);
