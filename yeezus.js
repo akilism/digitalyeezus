b417b5db31f426c105f309de990de61405d34cb8
Akil Harris <akil.harris@gmail.com>
2014-11-06 21:58:40 -0500

    server path fix

--- yeezus.js
+++ yeezus.js
@@ -6,6 +6,7 @@
 
 var port = process.env.PORT || 8088;
 var env_mode = process.env.NODE_ENV || 'dev';
+env_mode = dev;
 var express = require('express'),
     url = require('url'),
     handlers = require('./handlers.js'),
